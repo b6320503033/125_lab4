35bd8da7ee88ff352c92eea3a0e4f8bcd38bf363
b6320503033 <thida.sa@ku.th>
2021-03-01 17:51:59 +0700

    Create lab4_3.c

---
+++ lab4_3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char a[200];
+    scanf("%s",a);
+    for(i=0; i<strlen(a); i++)
+    {
+        if(a[i]==a[i+1])
+            continue;
+
+        printf("%c",a[i]);
+    }
+    return 0;
+}
